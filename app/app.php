b3374cae68fa6fa6da1034ceebabfe039a297821
Marika Allely <mballely@gmail.com>
2016-02-18 15:11:11 -0800

    add twig pages

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Game.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -8,9 +9,29 @@
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array(
-            // twig input associative array
+            'form' => true
         ));
     });
+
+    $app->get("/results", function() use ($app) {
+        if ($_GET['p1input'] == "rock") {
+            $p1input = 'rock';
+        } elseif ($_GET['p1input'] == "paper") {
+            $p1input = 'paper';
+        } elseif ($_GET['p1input'] == "scissors") {
+            $p1input = 'scissors';
+        }
+
+        $new_game = new Game;
+        $result = $new_game->outcome($p1input);
+
+        return $app['twig']->render('index.html.twig', array(
+            'message' => array(
+                'text' => $result
+            )
+        ));
+    });
+
 
     return $app;
 ?>
